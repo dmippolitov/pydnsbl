e4016909b815c6ed5dc01ed4d6f1aef75d6f726d
Dmitry Ippolitov <ippolitov87@gmail.com>
2017-05-07 23:41:10 +0300

    setup fixes

--- setup.py
+++ setup.py
@@ -6,21 +6,32 @@ import os
 import sys
 
 requirements = [
-    'aiodns',
+    'aiodns>=1.1.1,<1.2',
 ]
 
 
 setup(
     name='pydnsbl',
-    version='0.1',
+    version='0.5',
     description='Async dnsbl lists checker based on asyncio/aiodns.',
     url='https://github.com/dmippoltiov/pydnsbl/',
 
     author=u'Dmitry ippolitov',
     author_email='ippolitov87@gmail.com',
+    license='MIT',
 
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
 )
