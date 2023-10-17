79e5123ff3aebb95ebfd9161c5f810e2cf0a2b49
Ippolitov Dmitry <ippolitov87@gmail.com>
2023-10-17 12:32:57 +0300

    added tests for :: ipv6 convertion

--- pydnsbl/tests.py
+++ pydnsbl/tests.py
@@ -104,6 +104,7 @@ def test_ipv6_converting():
     # https://datatracker.ietf.org/doc/html/rfc5782#section-2.4
     checker = DNSBLIpChecker()
     assert checker.prepare_query('2001:db8:1:2:3:4:567:89ab') == "b.a.9.8.7.6.5.0.4.0.0.0.3.0.0.0.2.0.0.0.1.0.0.0.8.b.d.0.1.0.0.2"
+    assert checker.prepare_query('2600:2600::f03c:91ff:fe50:d2') == "2.d.0.0.0.5.e.f.f.f.1.9.c.3.0.f.0.0.0.0.0.0.0.0.0.0.6.2.0.0.6.2"
 
 
 ## COMPAT TESTS
